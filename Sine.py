6e669660ba50c2ade8e3dfb24e2f60ab08e6644b
Martynmedge <martyn.marsam.edge@gmail.com>
2026-05-08 12:51:01 +0800

    moved imports to top of code

--- Sine.py
+++ Sine.py
@@ -1,3 +1,4 @@
+import math
 print ("Sine Rule Calculator")
 print ("1 - Input to Calculate the length of a side")
 print ("2 - Input to Calculate an angle")
@@ -6,7 +7,6 @@ if choice == "1":
     a = float(input("Enter the length of side a: "))
     A = float(input("Enter the measure of angle A in degrees: "))
     B = float(input("Enter the measure of angle B in degrees: "))
-    import math
     A_rad = math.radians(A)
     B_rad = math.radians(B)
     b = (a * math.sin(B_rad)) / math.sin(A_rad)
@@ -16,7 +16,6 @@ elif choice == "2":
     a = float(input("Enter the length of side a: "))
     b = float(input("Enter the length of side b: "))
     A = float(input("Enter the measure of angle A in degrees: "))
-    import math
     A_rad = math.radians(A)
     B_rad = math.asin((b * math.sin(A_rad)) / a)
     B = math.degrees(B_rad)
