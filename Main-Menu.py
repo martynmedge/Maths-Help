78c79c9272156b6373e79ea9473e7c9023bd6939
Martynmedge <martyn.marsam.edge@gmail.com>
2026-05-10 11:43:05 +0800

    update main menu

--- Main-Menu.py
+++ Main-Menu.py
@@ -2,12 +2,14 @@ import math
 import Quadratic
 import Cosine
 import Sine
+import grades
 print("Main Menu")
 print("1. Quadratic Solver")
 print("2. Cosine Rule Calculator")
 print("3. Sine Rule Calculator")
-print("4. Exit")
-choice = input("Enter your choice (1-4): ")
+print("4. Grade Calculator")
+print("5. Exit")
+choice = input("Enter your choice (1-5): ")
 if choice == "1":
     Quadratic.run()
 elif choice == "2":
@@ -15,6 +17,8 @@ elif choice == "2":
 elif choice == "3":
     Sine.run()
 elif choice == "4":
+    grades.run()
+elif choice == "5":
     print("Exiting the program.")
 else:
-    print("Invalid choice. Please enter a number from 1-4.")
+    print("Invalid choice. Please enter a number from 1-5.")
